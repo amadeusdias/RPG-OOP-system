b59a1e3a022ec756fc1e40d1cf86c12058fb1591
amadeus <amadeusdias@hotmail.com>
2023-02-08 03:06:25 -0300

    requisito6

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,13 +1,15 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-export default interface Fighter {
+interface Fighter {
   lifePoints: number;
   strength: number;
   defense: number;
-  energy?: Energy
-
-  attack(enemy: Fighter): void;
+  energy?: Energy;
+  attack(enemy: Fighter | SimpleFighter): void;
   special?(enemy: Fighter): void;
   levelUp(): void;
-  reciveDamage(attackPoints: number): number;
+  receiveDamage(attackPoints: number): number;
 }
+
+export default Fighter;
